47f7f9e0845a4f230a635d0eaef8067673ac34e0
Christiam Camacho <christiam@hotmail.com>
2020-05-12 07:04:21 -0400

    Remove copyright, bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,11 @@
 # -- Project information -----------------------------------------------------
 
 project = 'ElasticBLAST'
-copyright = '2020, Christiam Camacho'
+html_show_copyright = False
 author = 'Christiam Camacho'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.3'
 
 
 # -- General configuration ---------------------------------------------------

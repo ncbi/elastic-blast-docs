c685002f72ebdaefe4497205491e0c92e1f0e177
Greg Boratyn <boratyng@ncbi.nlm.nih.gov>
2020-07-13 10:35:07 -0400

    Updated version to 0.0.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ html_show_copyright = False
 author = 'Christiam Camacho'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------

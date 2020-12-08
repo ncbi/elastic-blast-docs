369fe94c87726263a4a5b24167104676b9c01b11
Christiam Camacho <christiam@hotmail.com>
2020-12-08 16:13:22 -0500

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ html_show_copyright = False
 author = 'Christiam Camacho'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.19'
+release = '0.0.20'
 
 
 # -- General configuration ---------------------------------------------------

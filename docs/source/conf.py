1bd201bbb63a25e95a9a53825a0a1e5864278297
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2021-02-16 16:30:23 -0500

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ html_show_copyright = False
 author = 'Christiam Camacho'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.24'
+release = '0.0.25'
 
 
 # -- General configuration ---------------------------------------------------

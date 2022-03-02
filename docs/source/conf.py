5f01e45cf11d5fbfd5f7d973092d4cd422413f22
Christiam Camacho <christiam@hotmail.com>
2022-03-02 16:23:43 -0500

    Bump version for release 0.2.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ html_show_copyright = False
 author = 'Christiam Camacho'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.4'
 
 
 # -- General configuration ---------------------------------------------------

3cfa8eb1c7adab32758505996dac3648933a7754
Zaretskaya, Irena <zaretska@ncbi.nlm.nih.gov>
2025-02-19 17:06:13 -0500

    Added thumbs up/down WB-6581

--- docs/source/_static/init.js
+++ docs/source/_static/init.js
@@ -15,4 +15,25 @@ function bannerAdInit()
 
 jQuery( document ).ready(function() {
     bannerAdInit();
+    jQuery("#vote_helpful_yes,#vote_helpful_no").bind("click", function (e) {
+        if(jQuery(":target").length) {
+            //file:///U:/blast64/django/elastic-blast-sphynx/WB-5728-add-missing-static-files/elastic-blast-docs/docs/build/html/janitor.html#auto-shutdown-feature
+            var currSubSection = jQuery(":target").attr("id");        //#auto-shutdown-feature                 
+        }
+        var currSection = jQuery("div.body section").first().attr("id");
+        
+        var ref;
+        if(currSection && currSection.length > 0){
+            ref = "section=" + currSection;
+        }
+        if(currSubSection && currSubSection.length) {
+            if(ref != "") ref += "&";
+            ref += "subsection=" + currSubSection;                 
+        }   
+        if(ref) {
+            jQuery(this).attr("ref",ref);                 
+        }
+        jQuery(this).closest(".ncbi-vote-helpful").text('Thanks for your feedback')        
+        return false;   
+    });          
 });
